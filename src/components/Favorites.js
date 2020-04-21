81d3b8619d8405d7a4b13bec05770186e732570c
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-04-21 23:09:37 +0300

    removed store connection

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,26 +1,18 @@
-import { useEffect, Fragment } from 'react';
-import { inject, observer } from 'mobx-react';
+import React, { Fragment } from 'react';
 import Card from './movie/Card';
 
-const Favorites = ({ store }) => {
-    let favArr = [];
-
-    useEffect(() => {
-        favArr = JSON.parse(localStorage.getItem('favorites'));
-        store.setFavorites(favArr);
-    }, []);
-
+const Favorites = (props) => {
     return (
         <Fragment>
-            {store.favorites.length > 0 ? (
-                store.favorites.map((item) => {
+            {props.favArr && props.favArr.length > 0 ? (
+                props.favArr.map((item) => {
                     return <Card key={item.id} id={item.id} poster={item.poster} title={item.title} year={item.year} rating={item.rating} />;
                 })
             ) : (
-                <div className="display-3">You have no favorites.</div>
+                <div className="display-4 p-1 info">You have no favorites.</div>
             )}
         </Fragment>
     );
 };
 
-export default inject('store')(observer(Favorites));
+export default Favorites;
