36f9485651a41bb82dd0d7162e6dadaca53b31fd
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-04-21 23:06:48 +0300

    favorites component store connection

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,18 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { inject, observer } from 'mobx-react';
 import Navbar from '../components/Navbar';
 import Favorites from '../components/Favorites';
 
-const favorites = () => {
+const favorites = ({ store }) => {
+    useEffect(() => {
+        store.setFavorites(JSON.parse(localStorage.getItem('favorites')));
+    }, []);
+
     return (
         <Fragment>
             <Navbar />
             <div className="container">
                 <div className="row d-flex justify-content-center flex-row mt-3 ">
-                    <Favorites />
+                    <Favorites favArr={store.favorites} />
                 </div>
             </div>
         </Fragment>
     );
 };
 
-export default favorites;
+export default inject('store')(observer(favorites));
