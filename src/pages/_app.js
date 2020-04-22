f255c9c8b1ce1a6b4ff96df8f704fb1000349758
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-04-22 23:34:40 +0300

    navbar import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/style.scss';
 import { Provider } from 'mobx-react';
 import store from '../store/store';
+import Navbar from '../components/Navbar';
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <Provider store={store}>
+            <Navbar />
             <Component {...pageProps} />
         </Provider>
     );
