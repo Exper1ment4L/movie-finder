6a7b102339d4898a45e023c66850c24acd372c8d
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-04-22 23:34:53 +0300

    used next/link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faHeart } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg justify-content-center navbar-dark ">
-            <a className="navbar-brand" href="/">
-                <FontAwesomeIcon icon={faSearch} color="#007bff" /> Search
-            </a>
-            <a className="navbar-brand" href="/favorites">
-                <FontAwesomeIcon icon={faHeart} color="red" /> Favorites
-            </a>
+            <Link href="/">
+                <a className="navbar-brand">
+                    <FontAwesomeIcon icon={faSearch} color="#007bff" /> Search
+                </a>
+            </Link>
+            <Link href="/favorites">
+                <a className="navbar-brand">
+                    <FontAwesomeIcon icon={faHeart} color="red" /> Favorites
+                </a>
+            </Link>
         </nav>
     );
 };
